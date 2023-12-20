968cc761e2c458e77ad342ad3839bba9189a037b
wendymackennah <wendymackennah5@gmail.com>
2023-12-20 21:43:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+// code your solution heref
+  function superbowlWin(record) {
+ const winningYearObject = record.find(entry => entry.result === "W");
+  
+    // Check if a winning year was found
+  if (winningYearObject) {
+   return winningYearObject.year;
+   } else {
+     return undefined;
+    }
+  }
+  
